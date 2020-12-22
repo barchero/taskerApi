ad2662c68054abf0f5983893f398031b2fa0dc1e
barchero <christianbartrina@gmail.com>
2020-12-22 11:14:28 +0100

    Testing typeorm repository instead of query builder

--- src/infrastructure/mssql/repositories/workOrders/ShortWorkOrdersRepository.ts
+++ src/infrastructure/mssql/repositories/workOrders/ShortWorkOrdersRepository.ts
@@ -23,22 +23,56 @@ export class ShortWorkOrdersRepositoryImpl implements ShortWorkOrdersRepository 
 
     async listShortWorkOrders(query: QueryList<ShortWorkOrder>): Promise<Pagination<ShortWorkOrder>> {
         const queryBuilderAlias = 'shortWorkOrder'
-        const workOrdersQB = this.shortWorkOrderRepository
+/*        const workOrdersQB = this.shortWorkOrderRepository
             .createQueryBuilder(queryBuilderAlias)
-            .leftJoinAndSelect(`${queryBuilderAlias}.client`, `${queryBuilderAlias}.client`)
+            .leftJoinAndSelect(`${queryBuilderAlias}.client`, `${queryBuilderAlias}.client`)*/
 
-        if(query.where){
-            workOrdersQB.where(query.where);
+       /* if(query.where){
+            if(typeof query.where === 'string'){
+                workOrdersQB.where(queryBuilderAlias + '.' + query.where);
+            } else if(typeof query.where === 'object'){
+                workOrdersQB.where(query.where);
+            }
         }
 
         if(query.orderBy){
             workOrdersQB.orderBy(this.addAliasToOrderKeys('shortWorkOrder', query.orderBy));
         }
 
-        return await this.paginationRepository.paginate<ShortWorkOrder>(
+        if(query.pagination.limit){
+            workOrdersQB.limit(query.pagination.limit);
+        }
+
+        if(query.pagination.page) {
+            workOrdersQB.offset(query.pagination.limit*(query.pagination.page-1))
+        }
+
+/!*        return await this.paginationRepository.paginate<ShortWorkOrder>(
             workOrdersQB,
             query
-        );
+        );*!/
+        const [data, total] = await workOrdersQB.getManyAndCount();*/
+
+        const [data, total] = await this.shortWorkOrderRepository.findAndCount({
+            relations: ["client"],
+            where: query.where,
+            order: query.orderBy,
+            take: query.pagination.limit,
+            skip: (query.pagination.limit*(query.pagination.page-1))
+        });
+
+
+        const res = new Pagination<ShortWorkOrder>();
+        res.items = data;
+        res.meta = {
+            currentPage: query.pagination.page,
+            itemCount: data.length,
+            itemsPerPage: query.pagination.limit,
+            totalItems: total,
+            totalPages: Math.ceil(total/query.pagination.limit)
+        }
+
+        return res;
     }
 
 }
